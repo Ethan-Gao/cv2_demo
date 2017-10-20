77c5431f8d30989e11bf335b386bb36305e01ffe
Ethan-Gao <Mr.Ethan.Gao@gmail.com>
2017-10-20 11:29:26 +0800

    show image ok: default and gray together
    Signed-off-by: Ethan-Gao <Mr.Ethan.Gao@gmail.com>

--- images/show_img.py
+++ images/show_img.py
@@ -4,7 +4,9 @@ import cv2
 
 # 主函数
 if __name__ == "__main__":
-    img = cv2.imread('example.jpg', cv2.IMREAD_COLOR) # 原图
-    cv2.imshow('example', img)
+    img1 = cv2.imread('example.jpg', cv2.IMREAD_COLOR) # 原图
+    img2 = cv2.imread('example.jpg', cv2.IMREAD_GRAYSCALE) # 灰度图
+    cv2.imshow('example1', img1)
+    cv2.imshow('example2', img2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
